64793cf080eb8e095f8eef93a46363f356083cf1
beedit <bgroulef@pm.me>
2022-11-18 17:30:40 +0000

    minor changes to homepage

--- index.js
+++ index.js
@@ -3,8 +3,6 @@ VANTA.HALO({
     mouseControls: true,
     touchControls: false,
     gyroControls: false,
-    // minHeight: 1080,
-    // minWidth: 200.00,
     baseColor: "#dd18a5",
     backgroundColor: "#1e1e1e",
     size: 2
