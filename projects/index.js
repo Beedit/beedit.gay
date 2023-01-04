cde3b64f648c23853fd0ceaa2b1b166155c69726
beedit <bgroulef@pm.me>
2023-01-04 19:42:14 +0000

    projects site mostly done

--- projects/index.js
+++ projects/index.js
@@ -1,3 +1,19 @@
-const loadProject = (title, description, img_url) => {
-    console.log(`<div class=\"project-box-grid\"><div class=\"project-box-grid-title\">${title}</div><div class=\"project-box-grid-image\"><img src=\"${img_url}\" alt=\"${title}\"></div><div class=\"project-box-grid-text\">${description}</div></div>`)
+const template = (title, description, image, url) => {
+    return `<div class="project-box"><div class="project-box-grid"><div class="project-box-grid-title">${title}</div><div class="project-box-grid-image"><img src="${image}" alt="${title}"></div><div class="project-box-grid-text">${description}</div></div></div>`
 }
+
+
+const loadData = (data) => {
+    let string = ""
+    Object.keys(data).forEach(function(key) {
+        string = string + (template(key, data[key].description, data[key].image, data[key].url))
+    });
+    console.log(string)
+    document.getElementById("projects").innerHTML = string
+}
+
+fetch('./projects.json')
+.then((response) => response.json())
+.then((data) => {
+  loadData(data)
+});
