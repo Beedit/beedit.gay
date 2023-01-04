5226573d48b083110a45cadbb2238f22edc77b31
beedit <bgroulef@pm.me>
2023-01-04 18:02:16 +0000

    wowie projects page

---
+++ projects/index.js
@@ -0,0 +1,3 @@
+const loadProject = (title, description, img_url) => {
+    console.log(`<div class=\"project-box-grid\"><div class=\"project-box-grid-title\">${title}</div><div class=\"project-box-grid-image\"><img src=\"${img_url}\" alt=\"${title}\"></div><div class=\"project-box-grid-text\">${description}</div></div>`)
+}
